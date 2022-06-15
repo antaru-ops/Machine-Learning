e17fe91b842259abe5a9fbc736ad039aa516b28b
Andrew <73929070+antaru-ops@users.noreply.github.com>
2022-06-15 20:29:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ classifier_name = st.sidebar.selectbox("Select Classifier", ("Support Vector Mac
 
 def get_dataset(ds_name):
     ds_name == "Heart Disease"
-    ds = pd.read_csv(r'C:\Users\andre\Desktop\Jupyter Notebook\Data\heart.csv')
+    #ds = pd.read_csv(r'C:\Users\andre\Desktop\Jupyter Notebook\Data\heart.csv')
+    url="https://github.com/antaru-ops/Machine-Learning/blob/main/heart.csv"
+    ds=pd.read_csv(url)
     newds = ds.dropna()
     x = newds.iloc[:, :13]
     y = newds.iloc[:, -1]
